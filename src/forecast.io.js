01d001d258f84f28bf905c9112dd01f3d33f751e
Simon Feiden <si.feiden@googlemail.com>
2014-05-08 15:11:17 +0200

    added forecast.io.js

--- src/forecast.io.js
+++ src/forecast.io.js
@@ -10,7 +10,7 @@ Forecast.prototype = {
 		var endpoint = 'https://api.forecast.io/forecast/' + this._apikey;
 		endpoint += '/' + lat + ',' + long;
 
-		// if options parameter is initialised, check all possible parameters and modifiy endpoint
+		// if options parameter is initialised, check all possible parameters and modify endpoint
 		if ( options ) {
 
 			if ( options.time )
@@ -24,7 +24,7 @@ Forecast.prototype = {
 
 			if ( options.exclude ) {
 				endpoint += delim + 'exclude=';
-				if ( options.exclude instanceof Array )
+				if ( Array.isArray(options.exclude) )
 					endpoint += options.exclude.join(',');
 				else
 					endpoint += options.exclude;
@@ -36,26 +36,30 @@ Forecast.prototype = {
 				delim = '&';
 			}
 
+			// Not part of the forecast API!
+			// The current date is added as a GET parameter to the request URL to prevent the browser from caching results.
+			// If it works depends on the browser, do not rely on it.
 			if ( options.nocache )
-				endpoint += delim + (new Date().getTime());
+				endpoint += delim + 'nocache=' + (new Date().getTime());
 
 		}
 
 		var req = new XMLHttpRequest();
 		req.open('GET', endpoint, true);
 		req.onload = function () {
-			// The Forecast API sends a response header with an expiry date for the data for most combinations of parameters.
+			/* The Forecast API sends a response header with an expiry date for the data for most combinations of parameters.
 			// If the option is set and the expiry available, the option's callback will be called at expiration time.
 			
 			var expire = this.getResponseHeader('Expires');
-			if ( expire != null && options && options.expired instanceof Function ) {
+			if ( expire != null && options && typeof options.expired === 'function' ) {
 				var timeout = new Date(expire);
 				timeout = new Date() - timeout;
 
 				window.setTimeout(options.expired, timeout);
 			}
+			*/
 
-			var res = new Forecast.Response(this.responseText);
+			var res = Forecast.Response(this.responseText);
 			callback(res);
 		};
 		req.send();
@@ -63,306 +67,67 @@ Forecast.prototype = {
 };
 
 
+/**
+ * Response - An object containing all data of a Forecast request.
+ * The <code>hourly, daily</code> and <code>minutely</code> properties contain Data.Block objects.
+ * The <code>flags</code> property contains a Data.Flags object.
+ */
 Forecast.Response = function (apiJson) {
-	this._rawData = JSON.parse(apiJson);
-	this._currently = null;
-	this._minutely = null;
-	this._hourly = null;
-	this._daily = null;
-	this._alerts = null;
+	var data = JSON.parse(apiJson);
+
+	if ( data.minutely )
+		data.minutely = Forecast.Data.Block(data.minutely);
+
+	if ( data.hourly )
+		data.hourly = Forecast.Data.Block(data.hourly);
+
+	if ( data.daily )
+		data.daily = Forecast.Data.Block(data.daily);
+
+	if ( data.flags )
+		data.flags = Forecast.Data.Flags(data.flags);
+
+	return data;
 };
 
-Forecast.Response.prototype = {
-	constructor: Forecast.Response,
-
-	getLatitude: function () {
-		return this._rawData.latitude;
-	},
-
-	getLongitude: function () {
-		return this._rawData.longitude;
-	},
-
-	getTimezone: function () {
-		return this._rawData.timezone;
-	},
-
-	getOffset: function () {
-		return this._rawData.offset;
-	},
-
-	getCurrently: function () {
-		if ( this._currently == null )
-			this._currently = new Forecast.Data.Point(this._rawData.currently);
-		return this._currently;
-	},
-
-	getMinutely: function () {
-		if ( this._minutely == null )
-			this._minutely = Forecast.Data.Block(this._rawData.minutely);
-		return this._minutely;
-	},
-
-	getHourly: function () {
-		if ( this._hourly == null )
-			this._hourly = Forecast.Data.Block(this._rawData.hourly);
-		return this._hourly;
-	},
-
-	getDaily: function () {
-		if ( this._daily == null )
-			this._daily = Forecast.Data.Block(this._rawData.daily);
-		return this._daily;
-	},
-
-	getRawCurrently: function () {
-		return this._rawData.currently;
-	},
-
-	getRawMinutely: function () {
-		return this._rawData.minutely;
-	},
-
-	getRawHourly: function () {
-		return this._rawData.hourly;
-	},
-
-	getRawDaily: function () {
-		return this._rawData.daily;
-	},
-
-	getAlerts: function () {
-		if ( this._alerts == null )
-			this._alerts = this._rawData.alerts.map(function (v) {
-					return new Forecast.Data.Alert(v);
-				});
-
-		return this._alerts;
-	},
-
-	getFlags: function () {
-		return new Forecast.Data.Flags(this._rawData.flags);
-	}
-
-};
 
 Forecast.Data = {};
 
+/**
+ * Data.Block - An array containing Data.Point objects. The array has the additional properties summary and icon.
+ */
 Forecast.Data.Block = function (rawBlock) {
-	var block = rawBlock.data.map( function (v) {
-		return new Forecast.Data.Point(v);
-	});
+	var block = rawBlock.data;
 	block.summary = rawBlock.summary;
 	block.icon = rawBlock.icon;
 
 	return block;
 };
 
-
+/*
 Forecast.Data.Point = function (rawPoint) {
-	this._rawPoint	= rawPoint;
+	return rawPoint;
 };
-
-Forecast.Data.Point.prototype = {
-	constructor: Forecast.Data.Point,
-
-	getTime: function () {
-		return this._rawPoint.time;
-	},
-
-	getSummary: function () {
-		return this._rawPoint.summary;
-	},
-
-	getIcon: function () {
-		return this._rawPoint.icon;
-	},
-
-	getSunriseTime: function () {
-		return this._rawPoint.sunriseTime;
-	},
-
-	getSunsetTime: function () {
-		return this._rawPoint.sunsetTime;
-	},
-
-	getMoonPhase: function () {
-		return this._rawPoint.moonPhase;
-	},
-
-	getNearestStormDistance: function () {
-		return this._rawPoint.nearestStormDistance;
-	},
-
-	getNearestStormBearing: function () {
-		return this._rawPoint.nearestStormBearing;
-	},
-
-	getPrecipIntensity: function () {
-		return this._rawPoint.precipIntensity;
-	},
-
-	getPrecipIntensityMax: function () {
-		return this._rawPoint.precipIntensityMax;
-	},
-
-	getPrecipIntensityMaxTime: function () {
-		return this._rawPoint.precipIntensityMaxTime;
-	},
-
-	getPrecipProbability: function () {
-		return this._rawPoint.precipProbability;
-	},
-
-	getPrecipType: function () {
-		return this._rawPoint.precipType;
-	},
-
-	getPrecipAccumulation: function () {
-		return this._rawPoint.precipAccumulation;
-	},
-
-	getTemperature: function () {
-		return this._rawPoint.temperature;
-	},
-
-	getTemperatureMin: function () {
-		return this._rawPoint.temperatureMin;
-	},
-
-	getTemperatureMinTime: function () {
-		return this._rawPoint.temperatureMinTime;
-	},
-
-	getTemperatureMax: function () {
-		return this._rawPoint.temperatureMax;
-	},
-
-	getTemperatureMaxTime: function () {
-		return this._rawPoint.temperatureMaxTime;
-	},
-
-	getApparentTemperature: function () {
-		return this._rawPoint.apparentTemperature;
-	},
-
-	getApparentTemperatureMin: function () {
-		return this._rawPoint.apparentTemperatureMin;
-	},
-
-	getApparentTemperatureMinTime: function () {
-		return this._rawPoint.apparentTemperatureMinTime;
-	},
-
-	getApparentTemperatureMax: function () {
-		return this._rawPoint.apparentTemperatureMax;
-	},
-
-	getApparentTemperatureMaxTime: function () {
-		return this._rawPoint.apparentTemperatureMaxTime;
-	},
-
-	getDewPoint: function () {
-		return this._rawPoint.dewPoint;
-	},
-
-	getWindSpeed: function () {
-		return this._rawPoint.windSpeed;
-	},
-
-	getWindBearing: function () {
-		return this._rawPoint.windBearing;
-	},
-
-	getCloudCover: function () {
-		return this._rawPoint.cloudCover;
-	},
-
-	getHumidity: function () {
-		return this._rawPoint.humidity;
-	},
-
-	getPressure: function () {
-		return this._rawPoint.pressure;
-	},
-
-	getVisibility: function () {
-		return this._rawPoint.visibility;
-	},
-
-	getOzone: function () {
-		return this._rawPoint.ozone;
-	}
-};
-
 
 
 Forecast.Data.Alert = function (rawAlert) {
-	this._rawAlert = rawAlert;
+	return rawAlert;
 };
-
-Forecast.Data.Alert.prototype = {
-	constructor: Forecast.Data.Alert,
-
-	getTitle: function () {
-		return this._rawAlert.title;
-	},
-
-	getExpires: function () {
-		return this._rawAlert.expires;
-	},
-
-	getDescription: function () {
-		return this._rawAlert.description;
-	},
-
-	getUri: function () {
-		return this._rawAlert.uri;
-	}
-};
+*/
 
 
-
+/**
+ * Data.Flags - Contains all properties described in the documentation. Properties with hyphon are camel-cased:
+ * <code>darkskyUnavailable, darkskyStations, datapointStations, isdStations, lampStations, metarStations, metnoLicense</code>
+ */
 Forecast.Data.Flags = function (rawFlags) {
 	this._rawFlags = rawFlags;
-};
 
-Forecast.Data.Flags.prototype = {
-	constructor: Forecast.Data.Flags,
-
-	getDarkskyUnavailable: function () {
-		return this._rawFlags['darksky-unavailable'];
-	},
-
-	getDarkskyStations: function () {
-		return this._rawFlags['darksky-stations'];
-	},
-
-	getDatapointStations: function () {
-		return this._rawFlags['datapoint-stations'];
-	},
-
-	getIsdStations: function () {
-		return this._rawFlags['isd-stations'];
-	},
-
-	getLampStations: function () {
-		return this._rawFlags['lamp-stations'];
-	},
-
-	getMetarStations: function () {
-		return this._rawFlags['metar-stations'];
-	},
-
-	getMetnoLicense: function () {
-		return this._rawFlags['metno-license'];
-	},
-
-	getSources: function () {
-		return this._rawFlags.sources;
-	},
-
-	getUnits: function () {
-		return this._rawFlags.units;
-	}
+	rawFlags.darkskyUnavailable = rawFlags['darksky-unavailable'];
+	rawFlags.darkskyStations = rawFlags['darksky-stations'];
+	rawFlags.datapointStations = rawFlags['datapoint-stations'];
+	rawFlags.isdStations = rawFlags['isd-stations'];
+	rawFlags.lampStations = rawFlags['lamp-stations'];
+	rawFlags.metarStations = rawFlags['metar-stations'];
+	rawFlags.metnoLicense = rawFlags['metno-license'];
 };
